de64092a74042e6db4d57140cdaa7d5d723bc010
Lior Rozen <lior@win.com>
2013-10-15 14:50:50 +0300

    checking twitter

--- app.py
+++ app.py
@@ -1,41 +1,51 @@
 from flask import Flask, request, url_for
 import twilio.twiml
+import hashtag
 app = Flask(__name__)
+
+from twitter import *
 
 
 @app.route("/")
 def home():
-    return "It's ALIVE!?one"
+
+    # create twitter API object
+    twitter = Twitter()
+
+    # perform a basic search
+    # twitter API docs: https://dev.twitter.com/docs/api/1/get/search
+    query = twitter.search(q = "lazy dog")
+
+    # print how quickly the search completed
+    print "Search complete (%f seconds)" % (query["completed_in"])
+
+    # loop through each of my statuses, and print its content
+    for result in query["results"]:
+        print "(%s) @%s %s" % (result["created_at"], result["from_user"], result["text"])
 
 
 @app.route("/voice/", methods=['GET', 'POST'])
 def hello_monkey():
     """Respond to incoming requests."""
+
     resp = twilio.twiml.Response()
 
-    # resp.say("W.T.F?")
-
-    # return str(resp)
-
-    snd_file = url_for('static', filename='mario.mp3')
-    resp.play( snd_file )
     g = resp.gather( numDigits = 1, action = "/handle-key", method = "POST" )
-    g.say( "For foo press 1" )
-    g.pause( length = 2 )
-    g.say( "For bar press 2" )
+    g.say( "For hashtag aws, press 1" )
+    g.say( "For hashtag twilio, press 2" )
 
     return str(resp)
 
 
-@app.route("/handle-key", methods=['GET', 'POST'])
+@app.route( "/handle-key", methods=[ "GET", "POST" ] )
 def handle_key():
 
     resp = twilio.twiml.Response()
-    digit_pressed = request.values.get('Digits', None)
+    digit_pressed = request.values.get( "Digits", None )
 
     resp.say( "You pressed %s" % digit_pressed )
 
     return str(resp)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=1337)
+    app.run( host = "0.0.0.0", debug = True, port = 1337 )
