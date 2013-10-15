d9020900da241892240dd789a7bc905a37beed05
Lior Rozen <lior@win.com>
2013-10-15 11:54:20 +0300

    testing gather command

--- app.py
+++ app.py
@@ -11,9 +11,13 @@ def home():
 @app.route("/voice/", methods=['GET', 'POST'])
 def hello_monkey():
     """Respond to incoming requests."""
-    resp = twilio.twiml.Response()
-    resp.say("Hello Monkey")
-    return str(resp)
+    # resp = twilio.twiml.Response()
+    # resp.say("Hello Monkey")
+    # return str(resp)
+
+    resp = twilio.twiml.Gather()
+    ans = resp.say("Enter something")
+    return str(ans)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=1337)
