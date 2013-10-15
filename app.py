6d8c9f7e4c9a9a779ebb6bb52705c16c01f34ca2
Lior Rozen <lior@win.com>
2013-10-15 14:54:57 +0300

    twitter instance name

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ from twitter import *
 def home():
 
     # create twitter API object
-    twitter = twitter.Twitter()
+    t = twitter.Twitter()
 
     # perform a basic search
     # twitter API docs: https://dev.twitter.com/docs/api/1/get/search
-    query = twitter.search(q = "lazy dog")
+    query = t.search(q = "lazy dog")
 
     # print how quickly the search completed
     print "Search complete (%f seconds)" % (query["completed_in"])
