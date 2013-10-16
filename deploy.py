ea91d7db669b3123976990686dae2936e620e592
= <=>
2013-10-17 02:18:26 +0300

    added fabric dependency and remote deployment script

---
+++ deploy.py
@@ -0,0 +1,10 @@
+from fabric.api import env, run, cd
+# Kill previous process
+print "Killing previous process"
+env.host_string = "184.72.219.16"
+env.user = "ubuntu"
+repo_dir = "/home/ubuntu/hack/twilio-aws-hack"
+with cd( repo_dir ):
+    run( "git reset --hard" )
+    run( "git pull" )
+    run( "python setup.py" )
