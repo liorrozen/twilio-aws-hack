b24dae827f7cf8dd3e37aa2321dc46f7125bb648
= <=>
2013-10-17 01:52:43 +0300

    added "tests" lol

---
+++ tests.py
@@ -0,0 +1,17 @@
+import random
+import unittest
+
+class TestTwilioAwsHack( unittest.TestCase ):
+
+    def setUp( self ):
+        pass
+    
+    def tearDown( self ):
+        pass
+
+    def test_sample( self ):
+        # Here's where the magic happens!
+        self.assertEquals( True, True )
+
+if __name__ == '__main__':
+    unittest.main()
