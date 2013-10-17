3abffc38f3a774cf8fddc58b5d286b76f36fb0b5
= <=>
2013-10-17 21:25:07 +0300

    improved setup script

--- setup.py
+++ setup.py
@@ -1,8 +1,31 @@
 import sys
 import subprocess
+import argparse
 
-# install
-pipfile = "dependencies"
-cmd = "pip install -r %s --target site-packages" % pipfile
-print "Run: %s" % cmd
-subprocess.call( cmd, shell = True )
+class Setup( object ):
+
+    def __init__( self ):
+
+        parser = argparse.ArgumentParser( description='Defines environment and initializes dependencies.' )
+        parser.add_argument('action', metavar='install', type=str, help='Run this script')
+        self.args = parser.parse_args()
+
+    def parse_args( self ):
+        try:
+            return {
+                "install" : self.install
+            }[ self.args.action ]()
+        except KeyError:
+            print "Unknown command"
+
+
+    def install():
+        pipfile = "dependencies"
+        install_cmd = "pip install -r %s --target site-packages" % pipfile
+        subprocess.call( install_cmd, shell = True )
+
+    
+
+if __name__ == "__main__":
+    Setup().parse_args()
+
