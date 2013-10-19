9bd484de606bb771fefb6cc98517dae22c9128a5
= <=>
2013-10-19 15:24:35 +0300

    use init script

--- deploy.py
+++ deploy.py
@@ -1,7 +1,6 @@
 import site
 import sys
 import os
-import subprocess
 
 full_path = os.path.realpath(__file__)
 path, file = os.path.split(full_path)
@@ -15,8 +14,6 @@ env.user = "ubuntu"
 repo_dir = "/home/ubuntu/hack/twilio-aws-hack"
 
 with cd( repo_dir ):
-    # print "Killing previous process"
-    # run( "if [ -f 'pid.lock' ]; then cat pid.lock | xargs kill fi" )
 
     run( "git reset --hard" )
     run( "touch yeaaaaaaaa" )
@@ -27,6 +24,7 @@ with cd( repo_dir ):
     run( "source venv/bin/activate" )
 
     run( "python setup.py install" )
-    run( "nohup python app.py &" )
+    run( "service twilio-aws-hack stop" )
+    run( "service twilio-aws-hack start" )
 
 fabric.network.disconnect_all()
